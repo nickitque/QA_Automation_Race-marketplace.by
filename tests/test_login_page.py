a2d545631cb1e224ce92ebc559ce4279b090e471
Nikita <nickitque.39@gmail.com>
2024-06-03 14:19:03 +0200

    global update

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,8 +1,7 @@
 import pytest
 from pages.login_page import LoginPage
 
-
-link = "http://127.0.0.1:8000/login/"
+link = "https://race-marketplace.by/login/"
 
 
 @pytest.mark.critical_test
@@ -10,6 +9,52 @@ def test_guest_can_authenticate_tc_id1(browser):
     """User can authenticate and log out from the system."""
     page = LoginPage(browser, link)
     page.open()
-    page.fill_username_field_with_valid_data()
+    page.fill_login_form_with_valid_data()
+    page.click_submit_login_form_btn()
     page.click_user_dropdown_btn()
     page.click_logout_btn()
+
+
+@pytest.mark.critical_test
+def test_guest_cant_authenticate_with_invalid_username_tc_id2(browser):
+    """Try to send contact form using invalid username and valid password."""
+    page = LoginPage(browser, link)
+    page.open()
+    page.fill_login_form_with_invalid_username()
+    page.click_submit_login_form_btn()
+    page.check_error_message()
+
+
+@pytest.mark.critical_test
+def test_guest_cant_authenticate_with_invalid_password_tc_id3(browser):
+    """Try to send contact form using valid username and invalid password."""
+    page = LoginPage(browser, link)
+    page.open()
+    page.fill_login_form_with_invalid_password()
+    page.click_submit_login_form_btn()
+    page.check_error_message()
+
+
+def test_guest_cant_authenticate_with_invalid_password_tc_id3(browser):
+    """Try to send contact form using valid username and invalid password."""
+    page = LoginPage(browser, link)
+    page.open()
+    page.fill_login_form_with_invalid_password()
+    page.click_submit_login_form_btn()
+    page.check_error_message()
+
+
+def test_guest_cant_authenticate_without_password_tc_id4(browser):
+    """Try to send contact form without password."""
+    page = LoginPage(browser, link)
+    page.open()
+    page.fill_login_form_without_password()
+    page.click_submit_login_form_btn()
+
+
+def test_guest_cant_authenticate_without_username_tc_id5(browser):
+    """Try to send contact form without username."""
+    page = LoginPage(browser, link)
+    page.open()
+    page.fill_login_form_without_username()
+    page.click_submit_login_form_btn()
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -2,7 +2,7 @@ import time
 from pages.main_page import MainPage
 
 
-link = "http://127.0.0.1:8000/"
+link = "https://race-marketplace.by"
 
 
 def test_guest_get_status_code_200_tc_id1(browser):
@@ -60,44 +60,88 @@ def test_guest_can_see_10_items_tc_id8(browser):
     page.count_item_cards_qty()
 
 
-def test_guest_can_click_hero_about_us_btn_tc_id10(browser):
+def test_guest_can_click_go_to_marketplace_btn_tc_id9(browser):
     """Clicking the 'Go to marketplace' under the items."""
     page = MainPage(browser, link)
     page.open()
     page.click_go_to_marketplace_btn()
 
 
-def test_guest_should_see_blog_h2_tc_id11(browser):
+def test_guest_should_see_blog_h2_tc_id10(browser):
     """Checking the h2 blog title."""
     page = MainPage(browser, link)
     page.open()
     page.should_be_blog_h2()
 
 
-def test_guest_should_see_blog_descripton_tc_id12(browser):
+def test_guest_should_see_blog_description_tc_id11(browser):
     """Checking the h2 blog description text."""
     page = MainPage(browser, link)
     page.open()
     page.should_be_blog_description()
 
 
-def test_guest_should_see_blog_descripton_tc_id13(browser):
-    """Checking the blog counters text."""
+def test_guest_should_see_blog_counters_tc_id12(browser):
+    """Checking the blog counter numbers."""
     page = MainPage(browser, link)
     page.open()
     page.check_counters()
 
 
 def test_guest_can_click_go_to_blog_btn_tc_id15(browser):
+    """Clicking go to blog button."""
     page = MainPage(browser, link)
     page.open()
     page.click_go_to_blog_btn()
 
 
-def test_guest_can_check_blog_h2_tc_id16(browser):
+def test_guest_can_partners_images_and_links_tc_id17(browser):
+    """Checking the presence of partners images and asserting the links."""
     page = MainPage(browser, link)
     page.open()
-    page.check_our_partners_h2_text()
+    page.check_our_partners_images()
+    page.check_our_partners_links()
+
+
+def test_guest_tc_id16(browser):
+    page = MainPage(browser, link)
+    page.open()
+
+
+def test_guest_tc_id17(browser):
+    page = MainPage(browser, link)
+    page.open()
+
+
+
+def test_guest_tc_id18(browser):
+    page = MainPage(browser, link)
+    page.open()
+
+
+def test_guest_tc_id19(browser):
+    page = MainPage(browser, link)
+    page.open()
+
+
+def test_guest_tc_id20(browser):
+    page = MainPage(browser, link)
+    page.open()
+
+
+def test_guest_tc_id21(browser):
+    page = MainPage(browser, link)
+    page.open()
+
+
+def test_guest_tc_id22(browser):
+    page = MainPage(browser, link)
+    page.open()
+
+
+def test_guest_tc_id23(browser):
+    page = MainPage(browser, link)
+    page.open()
 
 
 def test_guest_should_see_login_link(browser):
